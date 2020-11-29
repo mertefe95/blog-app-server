f48f1e2837deb2cb7d1edc0421baec021af7b1d4
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-11-29 19:14:45 +0300

    Commit

--- src/routes/Post.js
+++ src/routes/Post.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const Post = require('../models/Post');
-
+const mongoose = require('mongoose');
 
 router.get('/posts', async (req, res) => {
     const posts = await Post.find();
