f986a0b56a8c31d591d77c500d6aaecf875d85d4
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-12-15 17:35:24 +0300

    sendVerificationEmail added to the route

--- src/routes/User.js
+++ src/routes/User.js
@@ -154,7 +154,7 @@ if (!digit.test(password) || !upperLetter.test(password)) {
         await user.save()
 
 
-            sendConfirmationEmail(user)
+            sendVerificationEmail(user)
 
         res.status(200).send('Successful registration. Please verify your email. ')
 } catch (err) {
